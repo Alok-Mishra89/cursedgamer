c1333623f2e8afce020ae7494795977bb4eb03ee
Alok Mishra <alok.mishra.am89@gmail.com>
2024-01-12 21:01:50 +0530

    App font added

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
@@ -7,9 +8,13 @@ class DashboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(
-          child: Text("Cursed Gamer"),
-        ),
+        title: const Center(
+            child: Text(
+          "Cursed Gamer",
+          style: TextStyle(
+              fontWeight: FontWeight.w900, fontSize: 35, fontFamily: 'Salsa'),
+        )),
+        backgroundColor: Colors.deepOrangeAccent,
       ),
     );
   }
