7900a18cb5c3a254bb66292da47d8bd5cc063a61
Alok Mishra <alok.mishra.am89@gmail.com>
2024-01-14 00:11:13 +0530

    Side Menu Bar

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -7,15 +7,62 @@ class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-            child: Text(
-          "Cursed Gamer",
-          style: TextStyle(
-              fontWeight: FontWeight.w900, fontSize: 35, fontFamily: 'Salsa'),
-        )),
-        backgroundColor: Colors.deepOrangeAccent,
-      ),
-    );
+        appBar: AppBar(
+          title: const Center(
+              child: Text(
+            "Cursed Gamer",
+            style: TextStyle(
+                fontWeight: FontWeight.w900, fontSize: 35, fontFamily: 'Salsa'),
+          )),
+          backgroundColor: Colors.amber,
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(color: Colors.amber),
+                child: CircleAvatar(
+                  radius: 34,
+                  backgroundColor: Colors.cyan,
+                  child: Text("Cursed Gamer"),
+                ),
+              ),
+              ListTile(
+                title: const Text('Home'),
+                onTap: () {
+                },
+              ),
+              ListTile(
+                title: const Text('Item 1'),
+                onTap: () {
+
+                },
+              ),
+              ListTile(
+                title: const Text('Item 2'),
+                onTap: () {
+                },
+              ),
+              ListTile(
+                title: const Text('Item 3'),
+                onTap: () {
+
+                },
+              ),
+              ListTile(
+                title: const Text('Item 4'),
+                onTap: () {
+                },
+              ),
+              ListTile(
+                title: const Text('Item 5'),
+                onTap: () {
+
+                },
+              )
+              ]
+          ),
+        ));
   }
 }
