5b7016d4b0efe63414dab7e6ad6528c5e2ff7067
Alok Mishra <alok.mishra.am89@gmail.com>
2024-01-14 19:29:16 +0530

    splash screen timing decreased to 1 seconds

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -13,25 +13,26 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-
-    Timer(const Duration(seconds: 2), () {
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => const DashboardScreen()));
+    Timer(const Duration(seconds: 1), () {
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>const DashboardScreen()));
     });
   }
 
-@override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: Colors.amber,
-        child: const Center(
-            child: Text(
-          'Cursed Gamer',
-          style: TextStyle(
-              fontSize: 45, fontWeight: FontWeight.w700, color: Colors.black),
-        )),
-      ),
-    );
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        body: Container(
+          color: Colors.amber,
+          child: const Center(
+              child: Text(
+                'Cursed Gamer',
+                style: TextStyle(
+                    fontSize: 45,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.black),
+              )),
+        ),
+      );
+    }
   }
-}
+
